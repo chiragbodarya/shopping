5f7a1bc98d8fb84fdac36ccf6b23635ae0c19c23
CHIRAG BODARYA <crbodarya05@gmail.com>
2024-01-02 22:17:46 +0530

    new repo

---
+++ shopping/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./component/navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-red-800">Welcome to the Shopping App</h1>
+    </>
+  );
+}
+
+export default App;
