ad7a6fc740c2af87c8aa67eb159d23a9b0e8ec33
CHIRAG BODARYA <crbodarya05@gmail.com>
2024-01-13 22:58:28 +0530

    change

--- shopping/src/component/addtocart.jsx
+++ shopping/src/component/addtocart.jsx
@@ -3,32 +3,48 @@ import { IoClose } from "react-icons/io5";
 
 const addtocart = () => {
   return (
-    <div className="w-[100%] max-w-96 bg-[#e8eefa] min-h-[100vh]">
-      <div className="flex justify-between items-center mx-4 py-4">
-        <div>
-          <span className="text-[28px] font-bold capitalize">shopping bag</span>
-          <span className="text-[18px] text-[#696969] ml-3">
-            (<span>2 items</span>)
-          </span>
-        </div>
-        <p className="text-[#303030] text-[20px]">
-          <IoClose />
-        </p>
-      </div>
-      <hr className="bg-gray-900 h-0.5" />
-      <div className="p-5 w-[100%]">
-        <div className="flex gap-5">
-          <img src="/img/lengha_1.webp" alt="" className="w-[30%]" />
+    <div className="w-[100%] max-w-96 bg-[#e8eefa] min-h-[100vh] flex flex-col justify-between">
+      <div>
+        <div className="flex justify-between items-center mx-4 py-4">
           <div>
-            <p className="text-[20px] font-bold text-[#000]">
-              pink moti and colorfull handwork
-            </p>
-            <p className="inline-block text-[20px] font-semibold text-[#4F80E1]">
-              $123
-            </p>
+            <span className="text-[28px] font-bold capitalize">
+              shopping bag
+            </span>
+            <span className="text-[18px] text-[#696969] ml-3">
+              (<span>2 items</span>)
+            </span>
+          </div>
+          <p className="text-[#303030] text-[20px]">
+            <IoClose />
+          </p>
+        </div>
+        <hr className="bg-gray-900 h-0.5" />
+        <div className="overflow-y- h-[80vh]">
+          <div className="p-5 w-[100%]">
+            <div className="flex gap-5">
+              <img src="/img/lengha_1.webp" alt="" className="w-[30%]" />
+              <div>
+                <p className="text-[20px] font-bold text-[#000]">
+                  pink moti and colorfull handwork
+                </p>
+                <p className="inline-block text-[20px] font-semibold text-[#4F80E1]">
+                  $123
+                </p>
+              </div>
+            </div>
+            <p className="border-t-2 border-[#000] my-4" />
           </div>
         </div>
-        <p className="border-t-2 border-[#000] my-4" />
+      </div>
+      <div className="p-5">
+        <p className="border-t-2 border-[#000]" />
+        <p className="flex justify-between items-center w-[100%] my-4">
+          <span className="text-[16px] text-[#505050]">subtotal</span>
+          <span className="text-[18px] font-bold text-[#000]">$45.80</span>
+        </p>
+        <button className="w-[100%] py-2 px-5 text-center text-[16px] sm:text-[17px] md:text-[18px] capitalize text-[#fff] bg-[#4F80E1]">
+          process to checkout
+        </button>
       </div>
     </div>
   );
