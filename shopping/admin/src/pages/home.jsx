4664ae366ddb79425e9dba2716565449f00ef670
CHIRAG BODARYA <crbodarya05@gmail.com>
2024-02-03 21:10:22 +0530

    change

--- shopping/admin/src/pages/home.jsx
+++ shopping/admin/src/pages/home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const home = () => {
-  return <div>this is home page</div>;
+  return (
+    <div>
+      <div className="flex items-center h-[200px]">
+        <div className="border-2 border-[#000] rounded-5"></div>
+        <div className="border-2 border-[#000] rounded-5"></div>
+      </div>
+    </div>
+  );
 };
 
 export default home;
