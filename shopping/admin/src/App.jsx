70facf6926ce84b79157130dd8cf84be7df3abe2
CHIRAG BODARYA <crbodarya05@gmail.com>
2024-02-14 14:19:20 +0530

    change

--- shopping/admin/src/App.jsx
+++ shopping/admin/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Loader from "react-loader-spinner";
+
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
@@ -7,24 +9,39 @@ import Home from "./pages/home";
 import NotFound from "./NotFound";
 
 function App() {
-  return (
-    <div className="h-full max-w-100vh">
-      <Navbar />
-      {/* <div className="flex pt-[60px] " style={{ height: "calc(100vh - 56px)" }}> */}
-      <div className="flex pt-[60px] h-[100vh]">
-        <Sidebar />
-        <main className="p-4 flex flex-col justify-between items-center w-full h-full">
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </Router>
-          <Footer />
-        </main>
-      </div>
+  render() {
+   return (
+    <div>
+      {this.state.loading ? (
+        <Loader
+          type="TailSpin"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} // Optional animation timeout in milliseconds
+        />
+      ) : (
+        <div className="h-full max-w-100vh">
+          <Navbar />
+          {/* <div className="flex pt-[60px] " style={{ height: "calc(100vh - 56px)" }}> */}
+          <div className="flex pt-[60px] h-[100vh]">
+            <Sidebar />
+            <main className="p-4 flex flex-col justify-between items-center w-full h-full">
+              <Router>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/*" element={<NotFound />} />
+                </Routes>
+              </Router>
+              <Footer />
+            </main>
+          </div>
+        </div>
+      )}
     </div>
   );
+}
+
 }
 
 export default App;
